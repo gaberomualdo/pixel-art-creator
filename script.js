3f836ba5156ee659908d3ab6940580703bdeb610
xtrp <36894883+xtrp@users.noreply.github.com>
2019-01-20 17:32:44 +0000

    started pixelart with working onclick version

---
+++ script.js
@@ -0,0 +1,38 @@
+function roundToNearest(num, numberToRound){
+   return Math.floor(num/parseFloat(numberToRound)) * numberToRound;
+}
+
+var canvas = document.querySelector("canvas");
+canvas.width = 720;
+canvas.height = 540;
+var context = canvas.getContext("2d");
+var gridSize = 30;
+
+function createGrid(){
+   context.lineWidth = 1;
+   context.strokeStyle = "#888";
+   for(var i = gridSize; i < canvas.width;i += gridSize){
+      context.beginPath();
+      context.moveTo(i, 0);
+      context.lineTo(i, canvas.height);
+      context.stroke();
+   }
+   for(var n = gridSize; n < canvas.height;n += gridSize){
+      context.beginPath();
+      context.moveTo(0, n);
+      context.lineTo(canvas.width, n);
+      context.stroke();
+   }
+}
+
+document.querySelector("canvas").onclick = function(e){
+   var coords = [e.clientX - (canvas.getBoundingClientRect().left), e.clientY - (canvas.getBoundingClientRect().top)];
+   var coords = [roundToNearest(coords[0], gridSize), roundToNearest(coords[1], gridSize)];
+
+   context.fillStyle = '#222';
+   context.rect(coords[0] + 1, coords[1] + 1, gridSize - 1, gridSize - 1);
+   context.fill();
+
+   console.log(coords);
+}
+createGrid();
